3727d3236dd37e1bc43ddcec1418f5c971cfea57
Kyle Wilson <ky.g.wilson@gmail.com>
2017-12-05 17:46:04 +0100

    adjusted def major_scale

--- musicscales.py
+++ musicscales.py
@@ -17,9 +17,13 @@ flats_dict = {
         6:'Gb', 7:'G', 8:'Ab', 9:'A', 10:'Bb', 11:'B'
         }
 
-tonic_flats = ['F' 'Bb', 'Eb', 'Ab', 'Db', 'Gb', 'Cb']
-tonic_sharps = ['C', 'G', 'D', 'A', 'E', 'B' 'F#', 'C#']
+# Initialize Lists for keys depending on the return of sharps or flats
+tonic_flats = ['F', 'Bb', 'Eb', 'Ab', 'Db', 'Gb', 'Cb']
+tonic_sharps = ['C', 'G', 'D', 'A', 'E', 'B', 'F#', 'C#']
 
+# Initializes lists for keyword search
+tonic_select_minor = ['Minor', 'minor', 'Natural Minor', 'natural minor']
+                
 def major_scale (tonic):
     """Tonic is the root of a scale, expressed in a string with either a # or
     a b representing sharp or flat.
@@ -28,7 +32,8 @@ def major_scale (tonic):
     """
     major_intervals = []
     major_notes = []
-
+    
+    # Searches tonic_flats in order to use the correct dictionary
     if tonic in tonic_flats:
         major_intervals.append(flats_dict[tonic])
     
@@ -41,7 +46,8 @@ def major_scale (tonic):
             else:
                 octave_shift = j - 12
                 major_notes.append(flats_dict[octave_shift])
-
+                
+    # Searches tonic_sharps in order to use the correct dictionary
     elif tonic in tonic_sharps:
         major_intervals.append(sharps_dict[tonic])
     
@@ -56,7 +62,23 @@ def major_scale (tonic):
                 major_notes.append(sharps_dict[octave_shift])
     else:
         return 'Invalid Input'
-            
+    
+    if tonic is 'Gb':
+        major_notes[3] = 'Cb'
+    elif tonic is 'Cb':
+        major_notes[0] = 'Cb'
+        major_notes[3] = 'Fb'
+    elif tonic is 'F#':
+        major_notes[6] = 'E#'
+    elif tonic is 'C#':
+        major_notes[6] = 'B#'
+        major_notes[2] = 'E#'
+
     return major_notes
-    
-    
+
+tonic_select = input("What note is the tonic of the scale?: ")
+scale_select = input("What scale? (Major or Natural Minor): ")
+if scale_select is "Major" or "major":
+    print(major_scale(tonic_select))
+else:
+    print("Invalid Command")
